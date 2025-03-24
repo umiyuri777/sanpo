960f9a9834f3e4f23d238e823344cac740c02900
umiyuri777 <yuseiai15@gmail.com>
2025-03-25 00:44:07 +0900

    feat: マップ操作を、拡大縮小時のみ回転なしに変更

--- lib/views/map.dart
+++ lib/views/map.dart
@@ -39,18 +39,21 @@ class _MapView extends State<MapView> {
       body: FlutterMap(
         options: const MapOptions(
           // 名古屋駅の緯度経度です。
-          initialCenter:
-              LatLng(35.170694, 136.881637),
+          initialCenter: LatLng(35.170694, 136.881637),
           initialZoom: 10.0,
           interactionOptions: InteractionOptions(
-            rotationWinGestures: MultiFingerGesture.rotate,
+            // 拡大縮小と回転を分離
+            rotationThreshold: 10.0, // 回転のための閾値を高く設定
+            enableMultiFingerGestureRace: true, // 複数指ジェスチャーの競合を有効化
+            rotationWinGestures: MultiFingerGesture.rotate, // 回転のみに設定
+            pinchZoomWinGestures: MultiFingerGesture.pinchZoom, // ピンチズームのみに設定
           ),
         ),
         children: [
           TileLayer(
             urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
           ),
-          const CurrentLocationLayer(), 
+          const CurrentLocationLayer(),
         ],
       ),
     );
