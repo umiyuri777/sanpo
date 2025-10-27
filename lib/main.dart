0a97a1642f9a24b3d282bfaaaeedc7f3afc3b22b
umiyuri777 <yuseiai15@gmail.com>
2025-10-27 15:26:37 +0900

    feat: ナビゲーションバーの実装

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const String title = 'Flutter location Demo';
 
-    return MaterialApp(
-      title: title,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      // home: const HomeView(title: title),
-      // home: const MapView(),
-      home: const CalendarView(),
-    );
+    return const MyStatefulWidget();
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  static const _screens = [
+    CalendarView(),
+    MapView(),
+    DebugLocationsView(),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: _screens[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.calendar_month), label: 'カレンダー'),
+            BottomNavigationBarItem(icon: Icon(Icons.map), label: 'マップ'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.bug_report), label: 'デバッグ'),
+          ],
+          type: BottomNavigationBarType.fixed,
+        ));
   }
 }
